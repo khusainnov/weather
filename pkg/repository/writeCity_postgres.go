2f4509a15581612e73f780a9317c4ca7ce9e38a3
Rustam <rustam.khusainov28@bk.ru>
2022-10-16 20:43:51 +0500

    improvement in postgres queries

--- pkg/repository/writeCity_postgres.go
+++ pkg/repository/writeCity_postgres.go
@@ -17,14 +17,27 @@ func NewWritePostgres(db *sqlx.DB) *WritePostgres {
 
 func (wr *WritePostgres) WriteCity(city string) (int, error) {
 	var id int
+	var exists bool
 	if city == "" {
 		return 0, errors.New("city line is empty")
 	}
-	query := fmt.Sprintf("INSERT INTO %s (city, counter) VALUES ($1, 1) RETURNING id;", searchQueries)
-	req := wr.db.QueryRow(query, city)
-	if err := req.Scan(&id); err != nil {
-		query = fmt.Sprintf("UPDATE %s SET counter=counter + 1 WHERE city=$1;", searchQueries)
-		_, err = wr.db.Query(query, city)
+
+	queryExist := fmt.Sprintf("SELECT exists (SELECT 1 FROM %s WHERE city=$1);", searchQueries)
+	queryCreate := fmt.Sprintf("INSERT INTO %s (city, counter) VALUES ($1, 1) RETURNING id;", searchQueries)
+	queryUpdate := fmt.Sprintf("UPDATE %s SET counter=counter + 1 WHERE city=$1;", searchQueries)
+
+	err := wr.db.QueryRow(queryExist, city).Scan(&exists)
+	if err != nil {
+		return 0, err
+	}
+
+	if exists == true {
+		_, err = wr.db.Query(queryUpdate, city)
+		if err != nil {
+			return 0, err
+		}
+	} else {
+		err = wr.db.QueryRow(queryCreate, city).Scan(&id)
 		if err != nil {
 			return 0, err
 		}
